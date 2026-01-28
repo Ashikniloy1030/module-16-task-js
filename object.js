dd52c43e283173c5cc33df37e9beb4d44439fae3
Ashikniloy1030 <ashiqurrhaman10@gmail.com>
2026-01-28 23:45:37 +0600

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+//অবজেক্ট এই ভাবে ডিক্লার করতে হয়। 
+//শেষে কমা ব্যবহার করতেই হবে।
+
+const man = {
+    name: 'Ashik',
+    height: '5 feet 8 inch',
+    weight: '83kg',
+    age: 30,
+    address: 'dhaka, Bangladesh',
+}
+
---
+++ reverse.js
@@ -0,0 +1,7 @@
+const sentence = '789';
+let reverse = ''
+for (const letter of sentence) {
+    //letter er jaygay onno j kono meaning full word use kora jabe
+    reverse = letter + reverse;
+}
+console.log(reverse);
